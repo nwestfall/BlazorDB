b0915f99b2e858cb6869f2eb870eb5f29f862d94
keg247 <44041557+keg247@users.noreply.github.com>
2023-02-15 19:58:28 +0000

    Migrate to net 6 (created new blazorDB.js file to fix strange error where it coulld not be found)

--- example/BlazorDB.Example/Program.cs
+++ example/BlazorDB.Example/Program.cs
@@ -1,57 +1,44 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using BlazorDB;
+using BlazorDB.Example;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-namespace BlazorDB.Example
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorDB(options =>
 {
-    public class Program
+    options.Name = "Test";
+    options.Version = 1;
+    options.StoreSchemas = new List<StoreSchema>()
     {
-        public static async Task Main(string[] args)
+        new StoreSchema()
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazorDB(options =>
-            {
-                options.Name = "Test";
-                options.Version = 1;
-                options.StoreSchemas = new List<StoreSchema>()
-                {
-                    new StoreSchema()
-                    {
-                        Name = "Person",
-                        PrimaryKey = "id",
-                        PrimaryKeyAuto = true,
-                        UniqueIndexes = new List<string> { "guid" },
-                        Indexes = new List<string> { "name" }
-                    }
-                };
-            });
-            builder.Services.AddBlazorDB(options =>
-            {
-                options.Name = "Test2";
-                options.Version = 1;
-                options.StoreSchemas = new List<StoreSchema>()
-                {
-                    new StoreSchema()
-                    {
-                        Name = "Item",
-                        PrimaryKey = "id",
-                        PrimaryKeyAuto = true,
-                        UniqueIndexes = new List<string> { "guid" },
-                        Indexes = new List<string> { "name" }
-                    }
-                };
-            });
-
-            await builder.Build().RunAsync();
+            Name = "Person",
+            PrimaryKey = "id",
+            PrimaryKeyAuto = true,
+            UniqueIndexes = new List<string> { "guid" },
+            Indexes = new List<string> { "name" }
         }
-    }
-}
+    };
+});
+builder.Services.AddBlazorDB(options =>
+{
+    options.Name = "Test2";
+    options.Version = 1;
+    options.StoreSchemas = new List<StoreSchema>()
+    {
+        new StoreSchema()
+        {
+            Name = "Item",
+            PrimaryKey = "id",
+            PrimaryKeyAuto = true,
+            UniqueIndexes = new List<string> { "guid" },
+            Indexes = new List<string> { "name" }
+        }
+    };
+});
+
+await builder.Build().RunAsync();
